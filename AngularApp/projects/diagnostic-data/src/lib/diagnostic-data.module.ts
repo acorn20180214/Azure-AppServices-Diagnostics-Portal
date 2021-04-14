8ce0fc13bb4d50075c3daa3fa986913d9be28569
XiaoxuHU <xiaoxhu@microsoft.com>
2021-04-13 17:34:16 -0700

    Refactor detector-time-picker

--- AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
+++ AngularApp/projects/diagnostic-data/src/lib/diagnostic-data.module.ts
@@ -83,7 +83,7 @@ import { AppDependenciesComponent } from './components/app-dependencies/app-depe
 import { HighchartsChartModule } from 'highcharts-angular';
 import { HighchartsGraphComponent } from './components/highcharts-graph/highcharts-graph.component';
 import { FabNavModule } from './components/fab-nav/fab-nav.module';
-import { FabPanelModule, FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule, FabDetailsListModule, FabTextFieldModule, FabMessageBarModule, FabButtonModule, FabTooltipModule, FabSpinnerModule, FabCalloutModule, FabCheckboxModule, FabPivotModule, FabLinkModule } from '@angular-react/fabric';
+import { FabPanelModule, FabIconModule, FabChoiceGroupModule, FabSearchBoxModule, FabDropdownModule, FabDetailsListModule, FabTextFieldModule, FabMessageBarModule, FabButtonModule, FabTooltipModule, FabSpinnerModule, FabCalloutModule, FabCheckboxModule, FabPivotModule, FabLinkModule, FabDatePickerModule, FabCalendarModule } from '@angular-react/fabric';
 import { SummaryCardsComponent } from './components/summary-cards/summary-cards.component';
 import { InsightsV4Component } from './components/insights-v4/insights-v4.component';
 import { CardSelectionV4Component } from './components/card-selection-v4/card-selection-v4.component';
@@ -116,6 +116,7 @@ import { HighChartsHoverService } from './services/highcharts-hover.service';
 import { RouterModule } from '@angular/router';
 import { FabDataTableComponent } from './components/fab-data-table/fab-data-table.component';
 import { SolutionPanelComponent } from './components/solution-panel/solution-panel.component';
+import { DetectorTimePickerComponent } from './components/detector-time-picker/detector-time-picker.component';
 
 @NgModule({
   imports: [
@@ -143,7 +144,9 @@ import { SolutionPanelComponent } from './components/solution-panel/solution-pan
     FabCalloutModule,
     FabCheckboxModule,
     FabPivotModule,
-    FabLinkModule
+    FabLinkModule,
+    FabDatePickerModule,
+    FabCalendarModule
   ],
   providers: [
     ClipboardService
@@ -190,7 +193,8 @@ import { SolutionPanelComponent } from './components/solution-panel/solution-pan
     CollapsibleListItemComponent,
     FabCardComponent,
     FabDataTableComponent,
-    SolutionPanelComponent
+    SolutionPanelComponent,
+    DetectorTimePickerComponent
   ],
   entryComponents: [DetectorListAnalysisComponent],
   exports: [
@@ -214,7 +218,8 @@ import { SolutionPanelComponent } from './components/solution-panel/solution-pan
     CollapsibleListComponent,
     CollapsibleListFabricComponent,
     CollapsibleListItemComponent,
-    FabTabComponent
+    FabTabComponent,
+    DetectorTimePickerComponent
   ],
 })
 export class DiagnosticDataModule {
